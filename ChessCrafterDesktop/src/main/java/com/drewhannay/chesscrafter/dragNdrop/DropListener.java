7819203638be1d1c24d788d70b8e8334c2688e98
i-Taozi <879714748@qq.com>
2022-02-19 22:36:53 +0800

    plusonelabs/calendar-widget:original code

--- ChessCrafterDesktop/src/main/java/com/drewhannay/chesscrafter/dragNdrop/DropListener.java
+++
@@ -1,5 +0,0 @@
-package com.drewhannay.chesscrafter.dragNdrop;
-
-public interface DropListener {
-    void dropped(DropEvent event, boolean fromDisplayBoard);
-}
--- ChessCrafterShared/src/main/java/com/drewhannay/chesscrafter/logic/PieceConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.drewhannay.chesscrafter.logic;
-
-public final class PieceConfiguration {
-    public int teamId;
-    public boolean isObjective;
-    public String internalId;
-}
