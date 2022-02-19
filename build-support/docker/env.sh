b169bf05857f1db4fa5b9962b75cf64009a8cf63
i-Taozi <879714748@qq.com>
2022-02-19 22:53:41 +0800

    greenrobot/greenDAO:original code

--- build-support/docker/env.sh
+++
@@ -1,5 +0,0 @@
-export gradleUserDir="${HOME}/.gradle"
-export gnupgUserDir="${HOME}/.gnupg"
-export userId=$(id -u)
-export groupId=$(id -g)
-export projectDir="$( cd "${scriptDir}"/../.. && pwd )"
